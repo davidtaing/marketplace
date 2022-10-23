8b7f50bb7d26d6d1c4726e23cc316a57bb37717f
davidtaing <adavidtaing@gmail.com>
2022-10-23 15:06:01 +1100

    feat: mobile NavMenu

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,13 +1,22 @@
 import { MdMenu, MdClose } from "react-icons/md";
+import { joinClassNames } from "../utils";
 
 interface HamburgerMenuProps {
   active: boolean;
   toggleActive: () => void;
+  className?: string;
 }
 
-export const HamburgerMenu = ({ active, toggleActive }: HamburgerMenuProps) => {
+export const HamburgerMenu = ({
+  active,
+  toggleActive,
+  className,
+}: HamburgerMenuProps) => {
   return (
-    <div className="hamburgerMenu h-7 w-7" onClick={toggleActive}>
+    <div
+      className={joinClassNames("hamburgerMenu h-7 w-7 z-50", className ?? "")}
+      onClick={toggleActive}
+    >
       {active ? (
         <MdClose className="w-full h-full" />
       ) : (
--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,20 +1,40 @@
 import Link from "next/link";
+import { joinClassNames } from "../utils";
 import { Button } from "./Button";
 
-export const NavMenu = () => {
+const styles = {
+  base: "flex flex-col gap-4 absolute top-0 left-0 z-40 p-4 w-screen h-screen bg-white",
+  mediaQueries: {
+    md: "md:static md:inline-flex md:flex-row md:gap-12 md:items-center md:bg-inherit md:w-auto md:h-auto md:p-0",
+  },
+};
+
+interface NavMenuProps {
+  active: boolean;
+  closeMenu: () => void;
+}
+
+export const NavMenu = ({ active, closeMenu }: NavMenuProps) => {
   return (
-    <ul className="inline-flex gap-12 items-center">
-      <li>
+    <ul
+      className={joinClassNames(
+        "navMenu",
+        styles.base,
+        styles.mediaQueries.md,
+        active ? "" : "hidden"
+      )}
+    >
+      <li onClick={closeMenu}>
         <Link href="/">
           <a className="align-middle">Home</a>
         </Link>
       </li>
-      <li>
+      <li onClick={closeMenu}>
         <Link href="/login">
           <a className="align-middle">Login</a>
         </Link>
       </li>
-      <li>
+      <li onClick={closeMenu}>
         <Link href="/signup">
           <a className="align-middle">
             <Button variant="SMALL_PRIMARY">Post a Task</Button>
