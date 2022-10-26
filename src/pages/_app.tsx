5c9c8e743c2dc3b546be103ddf8a512b2168b743
davidtaing <adavidtaing@gmail.com>
2022-10-26 15:51:04 +1100

    refactor: add Stripe setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ""
+);
 
 if (process.env.NODE_ENV === "development") {
   require("../mocks");
@@ -8,10 +14,12 @@ if (process.env.NODE_ENV === "development") {
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-sky-50 min-h-screen text-slate-700">
-      <Header />
-      <Component {...pageProps} />
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="bg-sky-50 min-h-screen text-slate-700">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </Elements>
   );
 }
 
