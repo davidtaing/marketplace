5d3402c9c902c98a5dcb8263a0362c1e6692286c
davidtaing <adavidtaing@gmail.com>
2022-10-30 15:27:49 +1100

    feat: add PageNotAvailableFallback to FeatureFlag
    to improve UX for Next.js pages in prod environments.

--- src/features/FeatureFlag/index.test.tsx
+++ src/features/FeatureFlag/index.test.tsx
@@ -26,6 +26,23 @@ describe("FeatureFlag", () => {
     });
   });
 
+  describe("When node_env = 'production' and Component isPage = true", () => {
+    test("it renders 'this feature is not available'", () => {
+      process.env = { ...env, NODE_ENV: "production" };
+
+      const ProductionComponent = () => <div>Production Component</div>;
+      const ActualComponent = FeatureFlag(ProductionComponent, {
+        isPage: true,
+      });
+
+      render(<ActualComponent />);
+
+      expect(
+        screen.queryByText(/this feature is not available./i)
+      ).not.toBeNull();
+    });
+  });
+
   describe("When node_env = 'development'", () => {
     test("it returns the React.Fragment", () => {
       process.env = { ...env, NODE_ENV: "development" };
