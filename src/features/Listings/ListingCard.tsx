6d4991fb7e8cfe2871c43b9aaaba1f70c3996c32
davidtaing <adavidtaing@gmail.com>
2022-11-04 11:22:18 +1100

    feat: add pointer cursor to ListingCards

--- src/features/Listings/ListingCard.tsx
+++ src/features/Listings/ListingCard.tsx
@@ -16,7 +16,10 @@ export const ListingCard = ({
   onClick,
 }: ListingCardProps) => {
   return (
-    <li className="border border-slate-600 p-4 font-semibold" onClick={onClick}>
+    <li
+      className="border border-slate-600 p-4 font-semibold cursor-pointer"
+      onClick={onClick}
+    >
       <h4 className="text-slate-900 text-l font-bold">{details}</h4>
       <h2 className="mt-4 text-slate-900 text-3xl">${budget}</h2>
       <div className="mt-3 flex justify-between">
